6c5aad601a298104181427a6c43c2700ec2721b8
Heryan Djaruma <heryandjaruma@gmail.com>
2025-01-21 13:50:51 +0700

    init spring boot

---
+++ src/main/java/dev/heryan/fibers_stack/FibersStackApplication.java
@@ -0,0 +1,13 @@
+package dev.heryan.fibers_stack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FibersStackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FibersStackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/heryan/fibers_stack/FibersStackApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.heryan.fibers_stack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FibersStackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
