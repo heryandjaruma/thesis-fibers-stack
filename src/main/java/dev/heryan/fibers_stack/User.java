b688d4893e7f72c4d43fac872b08bafc784f3b6a
Heryan Djaruma <heryandjaruma@gmail.com>
2025-04-25 01:32:22 +0700

    proper user model using lombok

---
+++ src/main/java/dev/heryan/fibers_stack/User.java
@@ -0,0 +1,25 @@
+package dev.heryan.fibers_stack;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.Date;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Document(collection = "users")
+public class User {
+    @Id String id;
+    private String username;
+    private String name;
+    private String email;
+    private String phone;
+    private String address;
+    private Date dateOfBirth;
+}
